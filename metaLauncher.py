0e8c044fa12885bf5e9d6faaf0ce3e5804c13a49
Dez <dez@lookingglassfactory.com>
2017-04-18 12:39:03 -0400

    added feature to quit out

--- metaLauncher.py
+++ metaLauncher.py
@@ -3,11 +3,16 @@ import subprocess
 
 while os.path.exists("unityLauncherPath.txt"):
 	if os.path.exists("targetApp.txt"):
-		targetAppPath = open("targetApp.txt", "r").readlines();
+		targetAppPath = open("targetApp.txt", "r").readlines()
 		
 		os.remove("targetApp.txt");  #delete the text file
+
+		targetAppPath = targetAppPath[0].strip()
+		#print(targetAppPath)
+		if targetAppPath == 'quit' :
+			quit()
 				
 		subprocess.call([targetAppPath])
 	else :
-		launcherPath = open("unityLauncherPath.txt", "r").readlines();
+		launcherPath = open("unityLauncherPath.txt", "r").readlines()
 		subprocess.call([launcherPath])
